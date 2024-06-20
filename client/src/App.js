8f848f4122f95c1e2def27cefc9f3ad3a46e28e6
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-06-21 05:11:00 +0530

    state and lifecycle

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './programmer.png';
 import './App.css';
 import UserProfile from './components/UserProfile';
+import Clock from './components/Clock';
 
 function App() {
 
@@ -12,7 +13,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to the User Profile Application</p>
+
+        <Clock/>
         
+        <br/>
         <UserProfile
           title="Display user profile"
           name="Ruwanthi Lakshika"
